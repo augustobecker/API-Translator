8348efd1b5f1db6239ea2041b6eb0b26052189f8
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2023-05-25 19:02:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import boto3
 from chalice import Chalice
+import os
+
+from utils_bucket.create_bucket import create_bucket
+from utils_bucket.delete_bucket import delete_bucket
+from utils_bucket.download_file_from_bucket import download_file
+from utils_bucket.list_buckets import list_buckets
+from utils_bucket.upload_file_to_bucket import upload_file
 
 app = Chalice(app_name='Translator')
-translate = boto3.client('translate-text')
+translate = boto3.client('translate')
 
 @app.lambda_function()
 def translate_text(event, context):
@@ -13,8 +20,8 @@ def translate_text(event, context):
 
     translate = boto3.client(
         'translate',
-        aws_access_key_id='AWS_ACCESS_KEY',
-        aws_secret_access_key='AWS_SECRET_ACCESS_KEY',
+        aws_access_key_id='ACCESS',
+        aws_secret_access_key='SECRET',
         region_name='us-east-1'
     )
 
@@ -25,4 +32,15 @@ def translate_text(event, context):
     )
 
     translated_text = response['TranslatedText']
+
+    create_bucket("History-Bucket")
+
+    file_name = os.path.abspath("utils_bucket/history/history.txt")
+
+    upload_file(file_name, "History-Bucket", "history.txt")
+
+    download_file("history.txt", "History-Bucket", "history.txt")
+
+
     return {'translated_text': translated_text}
+
